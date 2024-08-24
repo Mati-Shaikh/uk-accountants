4e24ca76b1e06dffddf9d621c0f878c20238ab58
Mati-Shaikh <matishaikh7@gmail.com>
2024-08-24 13:56:30 -0700

    Hero Section Updation

--- components/hero.js
+++ components/hero.js
@@ -1,4 +1,3 @@
-// ContentPictureComponent.jsx
 import React from 'react';
 // import './ContentPictureComponent.css'; // Uncomment if you have a CSS file
 
@@ -6,15 +5,15 @@ const ContentPictureComponent = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center lg:items-start p-4">
       {/* Content Section */}
-      <div className="lg:w-1/2 p-4 mt-8 lg:mt-32 flex flex-col justify-center text-center">
-        <h1 className="typing-animation mb-4 font-bold text-xl lg:text-2xl">Wise Numbers LTD</h1>
+      <div className="lg:w-1/2 p-4 mt-24 flex flex-col justify-center text-center">
+        <h1 className="typing-animation mb-4 font-bold text-xl lg:text-2xl mt-16">Wise Numbers LTD</h1>
         <p className="text-base lg:text-lg text-gray-700">
           Wise Numbers LTD, founded by Farhan Butt, a seasoned Chartered Accountant with Big 4 and Fortune 500 experience, is a family-oriented firm with a strong presence across the UK. Farhan diverse expertise drives the firm mission to offer top-tier financial guidance tailored to each client unique needs. Supported by a team of skilled bookkeepers, accountants, and tax experts, Wise Numbers serves a wide range of clients, from individual freelancers to large organizations. Their comprehensive services, including strategic financial consultancy and advanced tax planning, make them a trusted partner in achieving financial success and peace of mind.
         </p>
       </div>
       
       {/* Picture Section */}
-      <div className="lg:w-1/2 p-4 mt-8 lg:mt-16 flex justify-center">
+      <div className="lg:w-1/2 p-4 mt-8 sm:mt-16 lg:mt-16 flex justify-center">
         <img
           src="/gb.svg"
           alt="Description of Image"
